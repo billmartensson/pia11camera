c183290a9ee34fb3460ef09b3896a7d62becaac0
Bill Mårtensson <bill@magictechnology.se>
2022-11-10 09:10:09 +0100

    Initial Commit

---
+++ Pia11camera/Pia11cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pia11cameraApp.swift
+//  Pia11camera
+//
+//  Created by Bill Martensson on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Pia11cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
